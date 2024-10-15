96db541327b3c47a51a90535136e78ef21e629ff
serhio4smotrov <ser19061995@gmail.com>
2024-10-16 00:03:01 +0300

    initial

--- module_6_2.py
+++ module_6_2.py
@@ -1,35 +1,31 @@
 class Vehicle:
-    _COLOR_VARIANTS = ['зелёный', 'красный', 'розовый', 'жёлтый', 'чёрный']
+    __COLOR_VARIANTS = ['зелёный', 'красный', 'розовый', 'жёлтый', 'чёрный']
     def __init__(self, owner, model, engine_power, color):
         self.owner = owner
-        self._model = model
-        self._engine_power = engine_power
-        self._color = color
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = color
 
     def get_model(self):
-        return f'Модель: {self._model}'
+        return f'Модель: {self.__model}'
 
-    def get_horse_power(self):
-        return f'Мощность двигателя: {self._engine_power}'
+    def get_horsepower(self):
+        return f'Мощность двигателя: {self.__engine_power}'
 
     def get_color(self):
-        return f'Цвет: {self._color}'
+        return f'Цвет: {self.__color}'
 
     def print_info(self):
-        print(f'{self.get_model()}\n{self.get_horse_power()}\n{self.get_color()}\nВладелец: {self.owner}')
+        print(f'{self.get_model()}\n{self.get_horsepower()}\n{self.get_color()}\nВладелец: {self.owner}')
 
     def set_color(self, new_color):
-        is_prime = True
-        for i in self._COLOR_VARIANTS:
-            if i.lower() == new_color.lower():
-                self._color = new_color
-                is_prime = False
-                break
-        if is_prime:
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color.lower()
+        else:
             print(f'Нельзя сменить цвет на {new_color}')
 
 class Sedan(Vehicle):
-    _PASSENGERS_LIMIT = 5
+    __PASSENGERS_LIMIT = 5
 
 vehicle1 = Sedan('Дима', 'Toyota Mark II', 500, 'синий')
 vehicle1.print_info()
