1fa7a908711893120747bb3e787c80fa74c651cf
serhio4smotrov <ser19061995@gmail.com>
2024-10-17 13:39:53 +0300

    initial

--- module_6_3.py
+++ module_6_3.py
@@ -19,7 +19,8 @@ class Eagle:
 
 class Pegasus(Horse, Eagle):
     def __init__(self):
-        super().__init__()
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
     def move(self, dx, dy):
         self.run(dx)
